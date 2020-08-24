3bc925b55796a82bc359d17864f1d60ea5f29090
kenji <tora169mm@gmail.com>
2020-08-24 11:30:18 +0900

    [add] create a k8s deployment

--- main.tf
+++ main.tf
@@ -16,3 +16,56 @@ resource "kubernetes_service" "mysql_service" {
     }
     type = "NodePort"
 }
+
+resource "kubernetes_deployment" "mysql_deployment" {
+  metadata = {
+    name = "wordpress-mysql"
+    label = {
+        app = "${var.app_label}"
+    }
+  }
+
+  spec {
+    replicas = "1"
+
+    slector {
+    match_labels {
+        app ="${var.app_label}"
+        tier = "${var.mysql_tier}"
+        }
+    }
+
+    template {
+        metadata {
+            labels {
+                app = "${var.app_label}"
+                tier = "${var.mysql_tier}"
+            }
+        }
+        template {
+            metadata {
+                labels {
+                    app = "${var.app_label}"
+                    tier = "${var.mysql_tier}"
+                }
+            }
+            spec {
+                container {
+                    name = "mysql"
+                    image = "mysql:5.7"
+
+                    env {
+                        name = "MYSQK_ROOT_PASSWORD"
+                        value = "${var.mysql_password}"
+                    }
+                    port {
+                        container_port = "3306"
+                        name = "mysql"
+                    }
+                }
+            }
+        }
+    }
+}
+
+
